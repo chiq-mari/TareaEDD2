a7493e4c0fde22e5bf47fd687cb49399b8eeab58
chiq-mari <mariangelesmavarez@gmail.com>
2024-11-09 16:29:55 -0400

    docs

---
+++ Problema2/main.cpp
@@ -0,0 +1,5 @@
+/*2. Realizar un programa que procese los datos (Hacer algo con ellos, imprimir, jugar, etc.) 
+de una cola normal siguiendo un orden de prioridad (No utilizar colas de prioridad). Ejemplo:
+COLA:
+inicio-> (B, "Daniel"), (C, "Pablo"), (D, "Coraline"), (A, " Alfonzo" ), (C, "Lara"), (A, "Paula"), (A, "Chancho"), (A, "Luz")<- fin
+Procesar primero las A, después B, entonces C y D*/
